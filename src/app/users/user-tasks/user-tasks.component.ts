02c0db1c76e878beb388857c64fdce5571499268
Jagdale, Swaraj <Swaraj_Jagdale@dell.com>
2024-10-07 09:56:43 +0530

    Day 1 of Learning

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  OnInit,
+  computed,
+  inject,
+  input,
+} from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent implements OnInit {
+  userName = '';
+  userId = input.required<string>();
+  private usersServeice = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  // userName = computed(
+  //   () => this.usersServeice.users.find((u) => u.id === this.userId())?.name
+  // );
+
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) => {
+        this.userName =
+          this.usersServeice.users.find((u) => u.id === paramMap.get('userId'))
+            ?.name ?? '';
+      },
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+}
